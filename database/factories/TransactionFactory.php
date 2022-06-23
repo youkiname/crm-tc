5eaa883740caa2146a07309ab29717b994497485
youkiname <youkiname@mail.ru>
2022-06-23 13:11:19 +0300

    fix TransactionFacotry fields

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 use App\Models\User;
 use App\Models\Shop;
+use App\Models\ShoppingCenter;
 
 
 /**
@@ -23,7 +24,9 @@ class TransactionFactory extends Factory
         return [
             'seller_id' => User::inRandomOrder()->first()->id,
             'customer_id' => User::inRandomOrder()->first()->id,
+            'shopping_center_id' => ShoppingCenter::inRandomOrder()->first()->id,
             'shop_id' => Shop::inRandomOrder()->first()->id,
+            "bonuses_offset" => $this->faker->numberBetween($min = -200, $max = 200),
             "amount" => $this->faker->numberBetween($min = 5, $max = 400),
         ];
     }
