fde7dabe9a6c910ed6037acd08159aab152adb2c
youkiname <youkiname@mail.ru>
2022-06-23 13:10:58 +0300

    change verification code type from integer to string

--- database/migrations/2022_05_16_143111_create_verification_codes_table.php
+++ database/migrations/2022_05_16_143111_create_verification_codes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('verification_codes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->nullable()->references('id')->on('users')->onDelete('cascade');
-            $table->integer('code');
+            $table->string('code');
             $table->dateTime('expires_at');
             $table->timestamps();
         });
