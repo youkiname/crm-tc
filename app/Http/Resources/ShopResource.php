4851066690bc22bb5938a03eedfb707c2efa2090
youkiname <youkiname@mail.ru>
2022-06-24 21:11:28 +0300

    fix image links in resources

--- app/Http/Resources/ShopResource.php
+++ app/Http/Resources/ShopResource.php
@@ -10,12 +10,16 @@ class ShopResource extends JsonResource
 
     public function toArray($request)
     {
+        $avatarLink = 'https://picsum.photos/500/500';
+        if ($this->avatar_link) {
+            $avatarLink = 'https://api.top-sistem.ru' . $this->avatar_link;
+        }
         return [
             'id' => $this->id,
             'name' => $this->name,
             'cashback' => $this->cashback,
             'category' => new ShopCategoryResource($this->category),
-            'avatar_link' => $this->avatar_link ?? 'https://picsum.photos/500/500',
+            'avatar_link' => $avatarLink,
             'renter' => new RenterResource($this->renter)
         ];
     }
