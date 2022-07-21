78d281de4d4632799eee15ec15b99216f910d3b5
youkiname <youkiname@mail.ru>
2022-07-22 00:34:46 +0300

    fix UserResource

---
+++ app/Http/Resources/NestedShopResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NestedShopResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'cashback' => $this->cashback,
+            'category' => new ShopCategoryResource($this->category),
+        ];
+    }
+}
--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -26,7 +26,8 @@ class UserResource extends JsonResource
             'age' => $this->age(),
             'is_email_verified' => $this->isEmailVerified(),
             'avatar_link' => 'https://picsum.photos/500/500',
-            'shop' => $this->whenNotNull($this->jobShop()),
+            'shop' => new NestedShopResource($this->shop),
+            'jobShop' => new NestedShopResource($this->jobShop()),
             'shopping_center' => new ShoppingCenterResource($this->shoppingCenter()),
         ];
     }
